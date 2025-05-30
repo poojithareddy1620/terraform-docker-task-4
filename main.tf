fafae4f0f944fae061131321a96fced2f1cd749a
root <root@ip-172-31-94-30.ec2.internal>
2025-05-30 08:14:26 +0000

    Final task 4 submission with Terraform Docker setup

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx_container"
+  image = docker_image.nginx.name  # Use 'name' instead of 'latest'
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
